6acf70115383e55fdbec39c62a9e6b67e0d42a28
BSJJC <805086021@qq.com>
2025-02-15 23:53:46 +0800

    add: vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import "./style.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import router from "./routes/router.ts";
 import App from "./App.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 
 app.use(router);
+app.use(vuetify);
 app.mount("#app");
